bb22ffb9450e901afa211dbec907ef18e1979b1d
Fer Hernandez <fh.1111@gmail.com>
2018-11-04 21:43:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
 
- @@count
+ @@count = 0
  @@artist
 
   def initialize (name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
+
+    @@count += 1
 
   end
 
